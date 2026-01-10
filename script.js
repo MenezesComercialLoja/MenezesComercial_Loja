47323c15adb5f8af2200600a002e617a883fee39
MenezesComercialLoja <loja.menezcomercial@gmail.com>
2026-01-10 19:22:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.getElementById('notifyBtn').addEventListener('click', () => {
-    alert('Em breve você poderá receber novidades 😄');
-});
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide img');
+const totalSlides = slides.length;
+
+// Esconde todas e mostra a da vez
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.opacity = (i === index) ? "1" : "0";
+    });
+}
+
+// Função que avança o slide
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+}
+
+// Inicia mostrando a 1ª
+showSlide(currentIndex);
+
+// Troca a cada 4 segundos
+setInterval(nextSlide, 4000);
